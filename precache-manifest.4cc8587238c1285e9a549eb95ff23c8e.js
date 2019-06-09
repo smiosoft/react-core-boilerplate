48bba72d7ef6a5838f4cba4416f944fcad71f577
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-06-09 12:12:54 +0100

    Initial commit

---
+++ precache-manifest.4cc8587238c1285e9a549eb95ff23c8e.js
@@ -0,0 +1,61 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e94313bd4f93a0bca0785412640338d",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "092708eebe574a8a99d8",
+    "url": "/0.css"
+  },
+  {
+    "url": "/counter.82f40c7cd19e28f14b6b.chunk.js"
+  },
+  {
+    "revision": "5459a4c077fe765de093799335f008bc",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "de0869e324680c99efa1250515b4b41c",
+    "url": "/fonts/open-sans-v16-latin-regular.woff"
+  },
+  {
+    "revision": "33543c5cc5d88f5695dd08c87d280dfd",
+    "url": "/fonts/open-sans-v16-latin-regular.woff2"
+  },
+  {
+    "url": "/home.41361945947c894e4be7.chunk.js"
+  },
+  {
+    "url": "/icon_144x144.45c31223bdc08c1fd07833e9d3a3b138.png"
+  },
+  {
+    "url": "/icon_192x192.c2a5b79aa25f8dc2418c14fe419a85c0.png"
+  },
+  {
+    "url": "/icon_36x36.4009b7989c0c36b1295f1e4e0c87dde4.png"
+  },
+  {
+    "url": "/icon_48x48.449e4160783bf158e4ae75dbe27964c3.png"
+  },
+  {
+    "url": "/icon_512x512.f2c1886821e2857f46a42d82f16b8527.png"
+  },
+  {
+    "url": "/icon_72x72.27f29d3570fa1ee4db6463568155a00d.png"
+  },
+  {
+    "url": "/icon_96x96.272d1a99433e49d71cd4e9987b479600.png"
+  },
+  {
+    "url": "/main.9594dbc69613ad3727aa.chunk.js"
+  },
+  {
+    "url": "/manifest.b7ac1f2978b14d5ecd334e189d2c11a0.json"
+  },
+  {
+    "url": "/runtime~main.8b33b7d185786ddf93a3.js"
+  },
+  {
+    "url": "/vendor.092708eebe574a8a99d8.chunk.js"
+  }
+]);
