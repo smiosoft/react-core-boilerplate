6e723c5edbdde3e5c2624b8ef882d9e7a8940eaa
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-06-09 11:45:33 +0100

    Added svg logo to Home page

--- src/Boilerplate.Web/ClientApp/components/App/components/Home.jsx
+++ src/Boilerplate.Web/ClientApp/components/App/components/Home.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
 
+import Logo from '../assets/icons/logo.svg';
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  text-align: center;
 `;
 
 const Home = () => (
@@ -14,6 +17,7 @@ const Home = () => (
       <title>Home</title>
     </Helmet>
     <h2>Boilerplate for building React apps hosted with ASP.NET Core.</h2>
+    <Logo />
   </Container>
 );
 
