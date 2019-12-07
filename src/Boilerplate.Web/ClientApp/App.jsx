20a4f124d365454f5e09a7d1a6f2a75a461f7a0d
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2019-12-07 16:44:35 +0000

    Ported client over to new project

---
+++ src/Boilerplate.Web/ClientApp/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { hot } from 'react-hot-loader/root';
+import { Helmet } from 'react-helmet';
+import { Router } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+`;
+
+const App = () => {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then((registration) => {
+          // eslint-disable-next-line no-console
+          console.log('SW registered:', registration);
+        }).catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log('SW registration failed:', error);
+        });
+    });
+  }
+
+  return (
+    <Wrapper>
+      <Helmet titleTemplate="%s / React Core Boilerplate" defaultTitle="React Core Boilerplate" />
+      <h1>Test</h1>
+    </Wrapper>
+  );
+};
+
+export default hot(App);
