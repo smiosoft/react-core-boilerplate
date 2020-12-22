b5bea39082cd9c6ae12489c78368ab2a36c06ecf
Nick Smirnoff☕ <nick.smirnoff@live.co.uk>
2020-12-22 15:31:00 +0000

    Removed history package

--- src/Boilerplate.Web/ClientApp/App.jsx
+++ src/Boilerplate.Web/ClientApp/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
 import { Helmet } from 'react-helmet';
-import { Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import styled from 'styled-components';
 
-import history from './lib/history';
 import GlobalStyle from './GlobalStyle';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
@@ -35,11 +34,11 @@ const App = () => {
     <Wrapper>
       <Helmet titleTemplate="%s / React Core Boilerplate" defaultTitle="React Core Boilerplate" />
       <GlobalStyle />
-      <Router history={history}>
+      <BrowserRouter>
         <Navbar />
         <AppRouter />
         <Footer />
-      </Router>
+      </BrowserRouter>
     </Wrapper>
   );
 };
